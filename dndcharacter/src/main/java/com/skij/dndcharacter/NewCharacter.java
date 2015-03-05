c61a1e888074a8728da3d42b99ac3f65fe11f3f2
MLazz <michele.lazzeri@studenti.unimi.it>
2015-03-05 14:27:37 +0100

    MOving to desktop

---
+++ dndcharacter/src/main/java/com/skij/dndcharacter/NewCharacter.java
@@ -0,0 +1,48 @@
+package com.skij.dndcharacter;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import core.DNDCLASS;
+import core.DnDCharacterManipulator;
+
+
+public class NewCharacter extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_character);
+
+        //prova
+        int[] stats = {1, 1, 1, 1, 1, 1};
+        int[] sav = {1, 1, 1};
+        Utils.characterList.add(new DnDCharacterManipulator("Provanova", DNDCLASS.BARBARIAN, stats, 12, sav));
+        Utils.savePrefs(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_character, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
